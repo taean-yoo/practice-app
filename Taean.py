68e1f666e4cae44f6983a30a13db348b0ba66c35
taean-yoo <yootaean0115@unist.ac.kr>
2024-08-12 17:54:46 +0900

    Delete Taean.py

--- Taean.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.set_page_config(
-    page_title='Taean Project',
-    page_icon='https://i.namu.wiki/i/TCYHnImJHz7afWZWpS--GI0ecalq3wILFd-io09I1rs0vquk5CD38XvqwAvGyId1EsNwqTKvdg9bO1Zxap_984ROSyBkWk_pPcUwLNDDItzzGhK9VRT7zWZQCOfCdlpdNCV1qYvyDug_j58go9_Deg.webp'
-)
-st.title("chatGPT API 개발")
-st.subheader("아자아자 화이팅")
